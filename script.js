3c0d4598019b87a634bf4272d238879fd5fdf34c
L C <LF9904@gmail.com>
2025-01-09 21:05:04 -0500

    added coloring mouse draging and randomize spawns

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const {Engine, World, Bodies, Render, Runner} = Matter;
+const {Engine, World, Bodies, Render, Runner, MouseConstraint, Mouse} = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const {world} = engine;
@@ -6,14 +9,23 @@ const render = Render.create({
   element: document.body,
   engine,
   options: {
-    width: 800,
-    height: 600,
+    wireframes: false,
+    width,
+    height,
   },
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
+
+// Walls
 const walls = [
   Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
   Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
@@ -22,4 +34,22 @@ const walls = [
 ];
 
 World.add(world, walls);
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+
+// Random Shapes
+for (let i = 0; i < 30; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+        render: {
+          fillStyle: "lightblue",
+        },
+      })
+    );
+  }
+}
