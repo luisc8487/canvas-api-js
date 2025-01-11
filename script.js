61f716d789aa994f667758e47dad2a7111d44b13
L C <LF9904@gmail.com>
2025-01-10 21:08:46 -0500

    created algorthrim to shuffle through neighbor pairs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const {Engine, World, Bodies, Render, Runner, MouseConstraint, Mouse} = Matter;
+const {Engine, World, Bodies, Render, Runner} = Matter;
 
-const width = 800;
+const cells = 3;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -18,38 +19,66 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
-
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
-  Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
-  Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
-  Bodies.rectangle(800, 300, 40, 600, {isStatic: true}),
+  Bodies.rectangle(width / 2, 0, width, 40, {isStatic: true}),
+  Bodies.rectangle(width / 2, height, width, 40, {isStatic: true}),
+  Bodies.rectangle(0, height / 2, 40, height, {isStatic: true}),
+  Bodies.rectangle(width, height / 2, 40, height, {isStatic: true}),
 ];
 
 World.add(world, walls);
 
-// Random Shapes
-for (let i = 0; i < 30; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-        render: {
-          fillStyle: "lightblue",
-        },
-      })
-    );
+const shuffle = (arr) => {
+  let counter = arr.length;
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--;
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
   }
-}
+  return arr;
+};
+
+// Maze generation
+const grid = new Array(cells)
+  .fill(null)
+  .map(() => new Array(cells).fill(false));
+
+const verticals = new Array(cells)
+  .fill(null)
+  .map(() => new Array(cells - 1).fill(false));
+const horizontals = new Array(cells - 1)
+  .fill(null)
+  .map(() => new Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  // If we have visited the cell at [row, column], then return
+  if (grid[row][column]) {
+    return;
+  }
+  // Mark this cell as visited
+  grid[row][column] = true;
+  // Assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [row - 1, column],
+    [row, column + 1],
+    [row + 1, column],
+    [row, column - 1],
+  ]);
+  console.log(neighbors);
+  // See if neighbor is out of bounds
+
+  // If we have visited the neighbor, continue to next neighbor
+
+  // Remmove a wall from row or column
+
+  // Visit that next cell
+};
+
+stepThroughCell(1, 1);
+// console.log(grid);
